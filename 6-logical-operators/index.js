246ae7c4a58cce4be00274136d5a68f64f75cba4
hunghg255 <hunghg.agiletech@gmail.com>
2022-12-25 00:11:41 +0700

    6 logical operators

---
+++ 6-logical-operators/index.js
@@ -0,0 +1,17 @@
+// Logical Operators - Toán tử điều kiện
+
+// falsy: 0, 0n, -0, false, "", undefined, NaN, null
+// truthy:
+
+/**
+  &&  and
+  ||     or
+  !      not
+  ?:    Conditional (Ternary) Operator: toán tử 3 ngôi
+
+  ES6: ??
+ */
+
+const isShow = NaN ?? 2;
+
+console.log(isShow);
