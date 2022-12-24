56695e73b48542ed403b5d1e86bd5c1be6bf4959
hunghg255 <hunghg.agiletech@gmail.com>
2022-12-25 00:11:30 +0700

    5 so sanh

---
+++ 5-so-sanh-biến/index.js
@@ -0,0 +1,17 @@
+// So sánh biến trong javascript
+
+/**
+ *  8 kiểu so sánh
+  ==	 equal to
+  ===	equal value and equal type
+  !=	   not equal
+  !==	  not equal value or not equal type
+  >	     greater than
+  <	     less than
+  >=	  greater than or equal to
+  <=	  less than or equal to
+*/
+
+//  Exmaple
+const isEqual = 2 != '2';
+console.log('isEqual: ', isEqual);
